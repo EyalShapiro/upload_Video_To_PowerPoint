767ea420bb8551bb480f05a6a20946941068210b
EyalShapiro <eyalshapiro24@gmail.com>
2024-04-08 23:57:31 +0300

    cleaner code

--- upload_Vido_To_PPtX.py
+++ upload_Vido_To_PPtX.py
@@ -1,21 +1,19 @@
-from array import array
 import logging
 import os
 import threading
 from pptx import Presentation
 
-##const
-VIDEO_FOLDER = "test"
-PPTX_PATH = "test/my-PowerPoint.pptx"
-# -----------------
-logging.basicConfig(filename="app.log", level=logging.INFO)
+VIDEO_FOLDER = "folder1"
+PPTX_PATH = "Java-folder1.pptx"
+FILE_LOG = "app.log"
+logging.basicConfig(filename=FILE_LOG, level=logging.INFO)
 
 prs = Presentation()
 
 
+# לולאה על כל קובץ בתיקיית הוידאו
 def upload_Video_To_PPTX(video_name):
-    print("in app.log Record all events")
-
+    print(f"in {FILE_LOG}Record all events")
     for filename in os.listdir(video_name):
         if filename.endswith(".mp4"):
             video_path = os.path.join(video_name, filename)
@@ -32,15 +30,16 @@ def upload_Video_To_PPTX(video_name):
                     height=prs.slide_height,
                 )
                 logging.info(f"Uploaded file: {video_name_without_extension}")
-                print(f"Uploaded file: {video_name_without_extension}")
-            except Exception as e:
+            except Exception as err:
 
                 logging.error(
-                    f"Error uploading file {video_name_without_extension}: {e}"
+                    f"Error uploading file {video_name_without_extension}: {err}"
                 )
-                print(f"Error uploading file {video_name_without_extension}: {e}")
+    else:
+        logging.NOTSET(f"success")
 
 
+# שמירת המצגת
 def File_Is_Existed(PPTX_PATH):
     if os.path.exists(PPTX_PATH):
         print("Adding to an existing PowerPoint file:{PPTX_PATH}")
@@ -56,4 +55,5 @@ if __name__ == "__main__":
         thread.join()
         prs.save(PPTX_PATH)
     except Exception as e:
+        print(f"Error: {e}")
         logging.error(f"Error: {e}")
